f10c9142baafecf469dbdc908b53fb1a61fe6fb6
Som Sinha <somdsinha@gmail.com>
2024-07-19 11:24:26 +0200

    [refactor] use new harpoon command

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -27,7 +27,7 @@ return {
     end
 
     vim.keymap.set('n', '<leader>a', function()
-      harpoon:list():append()
+      harpoon:list():add()
     end)
 
     vim.keymap.set('n', '<leader>r', function()
