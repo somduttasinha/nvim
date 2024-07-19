0dab35e9fdd66d941ec9b322c96528c97b8d0dc3
Som Sinha <somdsinha@gmail.com>
2024-07-19 11:22:03 +0200

    [new] add rust and c support

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,31 +1,34 @@
 return {
-  'nvimtools/none-ls.nvim',
-  config = function()
-    local null_ls = require 'null-ls'
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.code_actions.shellcheck,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.markdownlint,
-        null_ls.builtins.diagnostics.actionlint,
-        null_ls.builtins.diagnostics.checkstyle.with {
-          extra_args = { '-c', '/Users/somsinha/.config/nvim/checkstyle/java_checks.xml' },
-        },
+    'nvimtools/none-ls.nvim',
+    config = function()
+        local null_ls = require 'null-ls'
+        null_ls.setup {
+            sources = {
+                null_ls.builtins.code_actions.shellcheck,
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.diagnostics.markdownlint,
+                null_ls.builtins.diagnostics.actionlint,
+                null_ls.builtins.diagnostics.checkstyle.with {
+                    extra_args = { '-c', '/Users/somsinha/.config/nvim/checkstyle/java_checks.xml' },
+                },
 
-        null_ls.builtins.diagnostics.ruff,
+                null_ls.builtins.diagnostics.ruff,
 
-        null_ls.builtins.formatting.google_java_format,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.golines,
-        null_ls.builtins.formatting.goimports_reviser,
+                null_ls.builtins.formatting.google_java_format,
+                null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.gofumpt,
+                null_ls.builtins.formatting.golines,
+                null_ls.builtins.formatting.goimports_reviser,
 
-        null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.black,
 
-        null_ls.builtins.formatting.fourmolu,
-      },
-    }
+                null_ls.builtins.formatting.fourmolu,
+                null_ls.builtins.formatting.clang_format,
 
-    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-  end,
+                null_ls.builtins.formatting.rustfmt,
+            },
+        }
+
+        vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+    end,
 }
