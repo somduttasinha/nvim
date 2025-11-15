be3eb927406f3fbf6785b2ff253362d3b0b33c8d
Som Sinha <som@dev.somsinha.com>
2025-11-15 13:59:51 +0000

    chore: add leader to leap prefix

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,7 +1,7 @@
 return {
     'ggandor/leap.nvim',
     config = function()
-        vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
-        vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+        vim.keymap.set({ 'n', 'x', 'o' }, '<leader>s', '<Plug>(leap)')
+        vim.keymap.set('n', '<leader>S', '<Plug>(leap-from-window)')
     end,
 }
